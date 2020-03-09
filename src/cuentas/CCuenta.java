f34bfd3b566b04ae096ac77668a90575436dc4ef
JaviCasti <castillejo.corral@gmail.com>
2020-03-09 18:25:18 +0100

    Encapsular variables

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -10,10 +10,45 @@ public class CCuenta {
 
 
   
-   String nombre;
-   String cuenta;
-   double saldo;
-   double tipoInterés;
+   public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	public void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+
+
+
+
+   private String nombre;
+   private String cuenta;
+   private double saldo;
+   private double tipoInterés;
 
     
     public CCuenta ()
